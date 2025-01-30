1eb9f87c471a02e77bded42b436f1e41e00b0746
Nassim Kalkoul <nkalkoul@bess-f2r6s6.clusters.42paris.fr>
2025-01-30 21:45:40 +0100

    first commit

---
+++ srcs/taken/ast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkalkoul <nkalkoul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 03:22:46 by nkalkoul          #+#    #+#             */
+/*   Updated: 2025/01/25 00:28:33 by nkalkoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+etape 1
+comment 
+1 _ 
+on cree une temp qui pointe sur s cmd 
+cette temp incremente : temp next_ 
+ on rencontre un operateur
+ on verifie son type si il fais partie des type prioritaire
+
+void	ft_seat(t_cmd *com)
+{
+}
