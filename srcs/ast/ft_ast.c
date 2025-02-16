ec5cda109b42c6ef45a866b92e4a5867279f0a0e
mounir <modavid@student.42.fr>
2025-02-17 00:30:22 +0100

    beforeast

--- srcs/ast/ft_ast.c
+++ srcs/ast/ft_ast.c
@@ -8,10 +8,21 @@ t_cmd	*ft_ast(t_cmd *first)
 {
 	t_cmd	*tmp;
 
-// ft_printcmd
 	tmp = ast_recursive(first, first->next);
 	print_AST_test(tmp);
 	return (tmp);
+}
+
+t_cmd	*first_pyramide(t_cmd *left, t_cmd *top)
+{
+	t_cmd	*right;
+
+	top->left = left;
+	left->top = top;
+	right = top->next;
+	top->right = right;
+	right->top = top;
+	return (ast_recursive(top, right->next));
 }
 
 t_cmd	*ast_recursive(t_cmd *left, t_cmd *top) {
@@ -20,15 +31,34 @@ t_cmd	*ast_recursive(t_cmd *left, t_cmd *top) {
 
 	if (top == NULL)
 		return (left);
-	top->left = left;
-	left->top = top;
-
-	right = top->next;
-
-	top->right = right;
-	right->top = top;
-
-	return (ast_recursive(top, right->next));
+	if (top->type == PIPE)
+	{
+		left->right = top;
+		top->top = left;
+		top->left = left->next;
+		left->next->top = top;
+		if (top->next->next && top->next->next->type == PIPE)
+		{
+			top->right = top->next->next;
+			top->next->next->top = top;
+			return (ast_recursive(top, top->next->next));
+		}
+		else
+		{
+			top->right = top->next;
+			top->next->top = top;
+			return (ast_recursive(left, top->next->next));
+		}
+	}
+	else
+	{
+		top->left = left;
+		left->top = top;
+		right = top->next;
+		top->right = right;
+		right->top = top;
+		return (ast_recursive(top, right->next));
+	}
 }
 
 //			  |
@@ -38,4 +68,28 @@ t_cmd	*ast_recursive(t_cmd *left, t_cmd *top) {
 //	  ls	cat
 
 
-// ls | cat | cat
+// ls || cat | cat | ws && ls
+
+// t_cmd	*ast_recursive(t_cmd *left, t_cmd *top) {
+
+// 	t_cmd	*right;
+
+// 	if (top == NULL)
+// 		return (left);
+// 	if (top->type)
+// 	{	
+
+
+
+
+// 		return (ast_recursive(rjhr, euge));
+// 	}
+// 	top->left = left;
+// 	left->top = top;
+// 	right = top->next;
+
+// 	top->right = right;
+// 	right->top = top;
+
+// 	return (ast_recursive(top, right->next));
+// }
--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -1,35 +1,5 @@
 #include "../../minishell.h"
 
-// int		ft_count_path(char	*pwd)
-// {
-// 	int		i;
-// 	int		count;
-
-// 	i = 1;
-// 	count = 0;
-// 	while (pwd[i] != '/')
-// 		i++;
-// 	i++;
-// 	while (pwd[i] && pwd[i] != '/')
-// 	{
-// 		i++;
-// 		count++;
-// 	}
-// 	return (count);
-// }
-
-// void	ft_path_cd(t_taken *current, t_global *global)
-// {
-// 	char	*pwd;
-// 	char	*path;
-
-// 	pwd = ft_getenv("PWD", global);
-// 	if (pwd == NULL)
-// 		return (1);
-// 	path = ft_malloc(sizeof(char) * (ft_count_path(pwd) + 1));
-// 	while ()
-// }
-
 int	ft_cd(t_taken *taken)
 {
 	t_taken	*current;
